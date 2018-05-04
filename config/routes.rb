95a0930927ae14b280eece73dbb1ee5d37f5b743
Mark Tice <marktice@protonmail.com>
2018-05-04 17:04:03 +1000

    Add reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,28 +3,28 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  get   '/contact',         to: 'pages#contact'
-  post  '/contact',         to: 'pages#contact_email'
+  get   '/contact',             to: 'pages#contact'
+  post  '/contact',             to: 'pages#contact_email'
 
+  get   '/profile',             to: 'profiles#show'
+  post  '/profile',             to: 'profiles#create'
+  get   '/profile/edit',        to: 'profiles#edit'
+  patch '/profile',             to: 'profiles#update'
 
-  get   '/profile',         to: 'profiles#show'
-  post  '/profile',         to: 'profiles#create'
-  get   '/profile/edit',    to: 'profiles#edit'
-  patch '/profile',         to: 'profiles#update'
+  get   '/passenger_bookings',  to: 'profiles#passenger_bookings'
+  get   '/driver_bookings',     to: 'profiles#driver_bookings'
 
-  get   '/passenger_bookings', to: 'profiles#passenger_bookings'
-  get   '/driver_bookings',    to: 'profiles#driver_bookings'  
+  get   '/car',                 to: 'cars#show'
+  post  '/car',                 to: 'cars#create'
+  get   '/car/edit',            to: 'cars#edit'
+  patch '/car',                 to: 'cars#update'
 
+  get   '/driver',              to: 'driver_profiles#show'
+  post  '/driver',              to: 'driver_profiles#create'
+  get   '/driver/edit',         to: 'driver_profiles#edit'
+  patch '/driver',              to: 'driver_profiles#update'
 
-  get   '/car',             to: 'cars#show'
-  post  '/car',             to: 'cars#create'
-  get   '/car/edit',        to: 'cars#edit'
-  patch '/car',             to: 'cars#update'
-
-  get   '/driver',          to: 'driver_profiles#show'
-  post  '/driver',          to: 'driver_profiles#create'
-  get   '/driver/edit',     to: 'driver_profiles#edit'
-  patch '/driver',          to: 'driver_profiles#update'
-
-  resources :bookings
+  resources :bookings do
+    resources :reviews, only: [:new, :create, :show]
+  end
 end
