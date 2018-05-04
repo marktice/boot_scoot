65d30b9243c0be739edfca32dbb2e655c7822ecd
Mark Tice <marktice@protonmail.com>
2018-05-04 18:19:06 +1000

    Add reviews controller and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,43 @@
 class ReviewsController < ApplicationController
   
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    booking = Booking.find(params[:booking_id])
+    @review = booking.reviews.build
+  end
+
+  def create
+    booking = Booking.find(params[:booking_id])
+    @review = booking.reviews.build(review_params)
+    
+    @review.reviewer = current_user
+
+    # set reviewee to other user of booking
+    if current_user == booking.passenger
+      @review.reviewee = booking.driver  
+    elsif current_user == booking.driver
+      @review.reviewee = booking.passenger
+    end
+
+    if @review.save!
+      flash[:success] = 'Review submitted, thanks for your feedback'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'Could not save review'
+      render 'new'
+    end
+
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit([
+      :rating, 
+      :content
+    ])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_many :passenger_bookings, class_name: 'Booking', foreign_key: 'passenger_id'
   has_many :driver_bookings,    class_name: 'Booking', foreign_key: 'driver_id'
 
-  has_many :reviews_of_user, class_name: 'Review', foreign_key: 'reviewee_id'
-  has_many :reviews_by_user, class_name: 'Review', foreign_key: 'reviewer_id'
+  has_many :reviews_of_user,    class_name: 'Review', foreign_key: 'reviewee_id'
+  has_many :reviews_by_user,    class_name: 'Review', foreign_key: 'reviewer_id'
 end
