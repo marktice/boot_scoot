2552b92e2515a1068f0777add57b7e3770e8f671
Mark Tice <marktice@protonmail.com>
2018-05-04 21:29:48 +1000

    Refactor average rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,31 @@ class User < ApplicationRecord
   has_one :car
   has_one :driver_profile
 
-  has_many :passenger_bookings, class_name: 'Booking', foreign_key: 'passenger_id'
-  has_many :driver_bookings,    class_name: 'Booking', foreign_key: 'driver_id'
+  has_many :passenger_bookings, class_name: 'Booking',  foreign_key: 'passenger_id'
+  has_many :driver_bookings,    class_name: 'Booking',  foreign_key: 'driver_id'
 
-  has_many :reviews_of_user,    class_name: 'Review', foreign_key: 'reviewee_id'
-  has_many :reviews_by_user,    class_name: 'Review', foreign_key: 'reviewer_id'
+  has_many :reviews_of,         class_name: 'Review',   foreign_key: 'reviewee_id'
+  has_many :reviews_by,         class_name: 'Review',   foreign_key: 'reviewer_id'
+
+  
+  def average_passenger_rating
+    ratings = []
+    self.reviews_of.each do |review|
+      if review.booking.passenger == self
+        ratings << review.rating
+      end
+    end
+    ratings.sum.to_f / ratings.length
+  end
+
+  def average_driver_rating
+    ratings = []
+    self.reviews_of.each do |review|
+      if review.booking.driver == self
+        ratings << review.rating
+      end
+    end
+    ratings.sum.to_f / ratings.length
+  end
+
 end
