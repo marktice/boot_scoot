7e9c264865b046aab7d2e176521f02263489f2dd
Mark Tice <marktice@protonmail.com>
2018-05-04 19:08:10 +1000

    Add average rating to profile as driver and passenger

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,4 +5,27 @@ class Profile < ApplicationRecord
 
   geocoded_by :home_address
   after_validation :geocode
+
+  def average_passenger_rating
+    ratings=[]
+    self.user.passenger_bookings.each do |booking|
+      unless booking.reviews == nil || booking.reviews.find_by(reviewee: self.user) == nil
+        ratings << booking.reviews.find_by(reviewee: self.user).rating
+      end
+    end
+    sum_ratings = ratings.sum.to_f
+    ave_rating = sum_ratings / ratings.length
+  end
+
+  # need 
+  def average_driver_rating
+    ratings=[]
+    self.user.driver_bookings.each do |booking|
+      unless booking.reviews == nil || booking.reviews.find_by(reviewee: self.user) == nil
+        ratings << booking.reviews.find_by(reviewee: self.user).rating
+      end
+    end
+    sum_ratings = ratings.sum.to_f
+    ave_rating = sum_ratings / ratings.length
+  end
 end
