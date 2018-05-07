8162ad2580727d7f7f0260116c984c7a549d60ea
Mark Tice <marktice@protonmail.com>
2018-05-07 17:16:09 +1000

    Remove booking index policy

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -5,10 +5,6 @@ class BookingPolicy < ApplicationPolicy
     end
   end
 
-  def index?
-    user.driver_profile != nil
-  end
-
   def show?
     user == record.passenger or user == record.driver
   end
