38fbd0ee827bc9bbbaec862d2c01b42276733f02
Mark Tice <marktice@protonmail.com>
2018-05-08 13:12:26 +1000

    Add phone number validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,5 +6,11 @@ class Profile < ApplicationRecord
   geocoded_by :home_address
   after_validation :geocode
 
-  validates :first_name, :last_name, :sex, :phone_number, :home_address, presence: true
+  validates :first_name, :last_name, :sex, presence: true
+
+  validates :home_address, presence: true
+
+  validates :phone_number, presence: true,
+            numericality: true,
+            length: { minimum: 10, maximum: 15 }
 end
